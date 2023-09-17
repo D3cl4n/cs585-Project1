34bbc384761c585504eb3c9d811cda03c36cfae0
RakeshVeetekat <rakesh.veetekat@gmail.com>
2023-09-17 17:06:18 -0400

    Changed Task A code to parse FaceIn.csv without StringTokenizer.

--- tasks/task1_code.java
+++ tasks/task1_code.java
@@ -11,46 +11,43 @@ import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.*;
+
 public class TaskA
 {
     public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable>
     {
         //String to write to output file "name + hobby"
         private final static Text output = new Text();
+        private final static IntWritable one = new IntWritable(1);
 
         public void map(Object key, Text value, Context context) throws IOException, InterruptedException
         {
-            StringTokenizer itr = new StringTokenizer(value.toString(), ",");
+            // Access the data from FaceIn.csv
+            // If the nationality is the same as a randomly selected nationality,
+            // then report the name and hobby of the current user
             String nationality = "test";
             String name = "";
             String hobby = "";
-            Bool flag = false;
-            int counter = 0;
+            String delimiter = ",";
+            File file = new File("dataset_generation\\FaceIn.csv");
+            FileReader fr = new FileReader(file);
+            BufferedReader br = new BufferedReader(fr);
+            String line = "";
+            String[] csvLine;
 
-            while (itr.hasMoreTokens())
-            {
-                if (counter == 1)
-                {
-                    name = itr.nextToken();
-                    counter = counter + 1;
+            while((line = br.readLine()) != null) {
+                csvLine = line.split(delimiter);
+                nationality = csvLine[2];
+                // Need to randomly pick a nationality to compare to this user's
+                if(nationality.equals("")) {
+                    name = csvLine[1];
+                    hobby = csvLine[4];
+                    output.set(name + "," + hobby);
+                    context.write(output,one);
                 }
-
-                else if (counter == 4)
-                {
-                    hobby = itr.nextToken();
-                    counter = counter + 1;
-                }
-
-                else if (itr.nextToken() == nationality)
-                {
-                    flag = true;
-                }
-                counter = counter + 1;
-            }
-
-            if (flag == true)
-            {
-                context.add(...);
             }
         }
     }
