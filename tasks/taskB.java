b7acce11d892bf504c675221f35859a469a7c3a8
RakeshVeetekat <rakesh.veetekat@gmail.com>
2023-09-23 12:58:13 -0400

    Task B can be tested with the WordCount code, but it still needs to be debugged.

--- tasks/taskB.java
+++ tasks/taskB.java
@@ -10,20 +10,21 @@ import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 // While iterating through each line of accesslogs.csv, need to keep track of the page just accessed.
 // Feed that information into the mapper and the reducer can be used to determine which page has been accessed the most.
 // We just need to order the final list that has the WhatPage number with the number of times it was accessed.
 // Then, we query FaceIn.csv with the WhatPage attribute since it's linked to the ID attribute there.
-public class taskB {
+public class TaskB {
 
     // List of tuples to store 10 highest WhatPage IDs with the number of times they were accessed
     private static String[][] mostPopularPages = new String[10][3];
     private static boolean popularPagesFilled = false;
     private static ArrayList<String> seenPageIDs = new ArrayList<String>();
     private static ArrayList<String> notSeenPageIDs = new ArrayList<String>();
-
     public static class FaceInMapper extends Mapper<Object, Text, Text, Text> {
         private Text outkey = new Text();
         private Text outvalue = new Text();
@@ -57,10 +58,11 @@ public class taskB {
     }
     public static class ReduceJoinReducer extends Reducer<Text, Text, Text, Text>{
         public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException{
+            Text result = new Text();
             int sum = 0;
             boolean fromAccessMapper = false;
             // Check if the values are from the Accesslogs Mapper
-            fromAccessMapper = val.toString().equals("1");
+            fromAccessMapper = values.iterator().next().toString().equals("1");
             if(fromAccessMapper){
                 // Input from AccesslogsMapper
                 for(Text val: values) {
@@ -115,11 +117,11 @@ public class taskB {
                     // Also, check if any of the IDs in notSeenPageIDs are in mostPopularPages
                     // If they are, then store the information and remove the ID from notSeenPageIDs
                     int minIndex = 0;
-                    int min = mostPopularPages[minIndex][1];
+                    String min = mostPopularPages[minIndex][1];
                     String[] notSeenPageIDsInfo;
                     for (int n = 0; n < mostPopularPages.length; n++) {
-                        if (Integer.valueOf(mostPopularPages[n][1]) < min) {
-                            min = Integer.valueOf(mostPopularPages[n][1]);
+                        if (Integer.valueOf(mostPopularPages[n][1]) < Integer.valueOf(min)) {
+                            min = mostPopularPages[n][1];
                             minIndex = n;
                         }
                         // Check to see if the page ID hasn't been seen before
@@ -135,34 +137,53 @@ public class taskB {
                             }
                         }
                     }
-                }
-                // Compare the current page's popularity with the page that has the lowest popularity in the list
-                // Replace the page ID and count in that row of mostPopularPages if the current page is more popular
-                if(Integer.toString(mostPopularPages[minIndex][1]) < sum) {
-                    mostPopularPages[minIndex][0] = key.toString();
-                    mostPopularPages[minIndex][1] = Integer.toString(sum);
+                    // Compare the current page's popularity with the page that has the lowest popularity in the list
+                    // Replace the page ID and count in that row of mostPopularPages if the current page is more popular
+                    if(Integer.valueOf(mostPopularPages[minIndex][1]) < sum) {
+                        mostPopularPages[minIndex][0] = key.toString();
+                        mostPopularPages[minIndex][1] = Integer.toString(sum);
+                    }
                 }
                 seenPageIDs.add(key.toString());
             }
             // Will write the mostPopularPages array into the context
             // The last time the reducer writes to the context will be when the array has the 10 most popular pages
-            // context.write(key, result);
-            context.write(1,mostPopularPages);
+            result.set(convertPagesArrayToText(mostPopularPages));
+            context.write(new Text("Most Popular Pages"),result);
         }
+    }
+
+    private static Text convertPagesArrayToText(String[][] pages) {
+        String result = "";
+        for(int n = 0;n < pages.length;n++) {
+            result += pages[n][2] + "\n";
+        }
+        return new Text(result);
+    }
+
+    public void debug(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "Task B");
+        job.setJarByClass(TaskB.class);
+        job.setReducerClass(ReduceJoinReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, FaceInMapper.class);
+        MultipleInputs.addInputPath(job, new Path(args[1]),TextInputFormat.class, AccesslogsMapper.class);
+        FileOutputFormat.setOutputPath(job, new Path(args[2]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
         Job job = Job.getInstance(conf, "Task B");
-        job.setJarByClass(taskB.class);
-        job.setMapperClass(TokenizerMapper.class);
-        job.setCombinerClass(IntSumReducer.class);
-        job.setReducerClass(IntSumReducer.class);
+        job.setJarByClass(TaskB.class);
+        job.setReducerClass(ReduceJoinReducer.class);
         job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(IntWritable.class);
-        FileInputFormat.addInputPath(job, new Path(args[1]));
+        job.setOutputValueClass(Text.class);
+        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, FaceInMapper.class);
+        MultipleInputs.addInputPath(job, new Path(args[1]),TextInputFormat.class, AccesslogsMapper.class);
         FileOutputFormat.setOutputPath(job, new Path(args[2]));
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
-
 }
