4753c680dbea2a7b4b28175cbc19b56e102a8b75
D3cl4n <dpmurphy@wpi.edu>
2023-09-25 16:54:44 -0400

    correct task E code

--- Submission/tasks_code/TaskE.java
+++ Submission/tasks_code/TaskE.java
@@ -1,108 +1,109 @@
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.apache.hadoop.yarn.webapp.hamlet.Hamlet;
+import org.apache.hadoop.io.IntWritable;
 
-
-public class TaskE
-{
-    public static class FaceInMapper extends Mapper<Object, Text, Text, Text>
-    {
-        private Text outKey = new Text();
-        private Text outValue = new Text();
-        private Text charLabel = new Text();
-
-        public void map(Text key, Text value, Context context) throws IOException, InterruptedException
-        {
-            System.out.println("here1");
-            if (!value.toString().contains("ID")) {
-                String delimiter = ",";
-                String line = value.toString();
-                String[] elements = line.split(delimiter);
-                outKey.set(elements[0]);
-                charLabel.set("F");
-                outValue.set(charLabel);
-                context.write(outKey, outValue);
-            }
-        }
-    }
-
-    public static class AccessMapper extends Mapper<Object, Text, Text, Text>
-    {
-        private Text outKey = new Text();
-        private Text outValue = new Text();
-        private Text outLabel = new Text();
-
-        public void map(Text key, Text value, Context context) throws IOException, InterruptedException
-        {
-            if (!value.toString().contains("AccessID"))
-            {
-                String delimiter = ",";
-                String line = value.toString();
-                String[] elements = line.split(delimiter);
-                outKey.set(elements[1]);
-                outLabel.set("A");
-                outValue.set(outLabel + elements[2]);
-                context.write(outKey, outValue);
-            }
-        }
-    }
-    public static class ReduceJoinReducer extends Reducer<Text,Text,Text,Text>
-    {
-        public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException
-        {
-            ArrayList<Integer> uniquePages = new ArrayList<Integer>();
-            int totalVisits = 0;
-            boolean flag = false;
-            for (Text val : values)
-            {
-                if (val.toString().contains("A"))
-                {
-                    String currValue = val.toString().replace("A", "");
-                    for (int x : uniquePages)
-                    {
-                        if (x == Integer.parseInt(currValue))
-                        {
-                            flag = true;
-                        }
-                    }
-
-                    if (!flag)
-                    {
-                        uniquePages.add(Integer.parseInt(currValue));
-                    }
-                    totalVisits = totalVisits + 1;
+public class taskEtest {
+    public static class FaceInMapper extends Mapper<Object, Text, Text, Text> {
+        private Text outkey = new Text();
+        private Text outvalue = new Text();
+        public void map (Object key, Text value, Context context)
+                throws IOException, InterruptedException{
+            if (!isHeader(value)) {
+                String[] parts = value.toString().split(",");
+                if (parts.length >= 2) {
+                    outkey.set(parts[0]);
+                    outvalue.set("Name\t" + parts[1]); //concanate the name
+                    context.write(outkey, outvalue);
                 }
             }
-            Text finalText = new Text("Unique Pages: " + uniquePages + "Total Visits" + totalVisits);
-            context.write(key, finalText);
+
+        }
+        private boolean isHeader(Text value) {
+            return value.toString().startsWith("ID,Name,Nationality,Country Code,Hobby");
         }
     }
 
-    public static void main(String[] args) throws Exception
-    {
-        System.out.println("start");
-        Configuration conf = new Configuration();
-        Job job = Job.getInstance(conf, "Task E");
-        job.setJarByClass(TaskE.class);
+    public static class AccessMapper extends Mapper<Object, Text, Text, Text> {
+        private Text perId = new Text();
+        private Text accessID = new Text();
+        //private datetime accessDate = new
+        public void map (Object key, Text value, Context context)
+                throws IOException, InterruptedException{
+            if (!isHeader(value)) {
+                String[] parts = value.toString().split(",");
+                if (parts.length >= 2) {
+                    perId.set(parts[1]);  // fk
+                    accessID.set("AccessCount\t" + parts[0]); //relation id as value
+                    context.write(perId, accessID);
+                }
+            }
+
+        }
+        private boolean isHeader(Text value) {
+            return value.toString().startsWith("AccessId, ByWho,WhatPage,TypeOfAccess,AccessTime");
+        }
+    }
+    public static class ReduceJoinReducer extends Reducer<Text, Text, Text, Text>{
+        private Text pName = new Text();
+        public void reduce(Text key, Iterable<Text> values, Context context)
+                throws IOException, InterruptedException{
+           // ArrayList<Integer> uniquePages = new ArrayList<Integer>();
+            Set<String> uniquePages = new HashSet<>();
+            String name = null;
+            int totalVisits=0;
+            boolean flag = false;
+            for (Text value : values) {
+                String[] parts = value.toString().split("\t");
+                if (parts.length == 2) {
+                    String dataType = parts[0];
+                    String dataValue = parts[1];
+
+                   if (dataType.equals("AccessCount"))
+                    {
+                        uniquePages.add(value.toString());
+                        totalVisits++;
+                    }
+                   else if (dataType.equals("Name"))
+                   {
+                       name = dataValue;
+                   }
+                }
+            }
+
+            if (name != null) {
+                pName.set(name);
+                int distinctCount = uniquePages.size();
+                Text finalText = new Text("Unique Pages: " + distinctCount + " Total Visits" + totalVisits);
+                context.write(pName,finalText);
+            }
+        }
+    }
+    public static void main(String[] args) throws Exception {
+        Configuration conf= new Configuration();
+        Job job=new Job(conf,"Task E");
+        job.setJarByClass(taskG.class);
         job.setReducerClass(ReduceJoinReducer.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(Text.class);
-        MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, FaceInMapper.class);
-        MultipleInputs.addInputPath(job, new Path(args[2]), TextInputFormat.class, AccessMapper.class);
-        FileOutputFormat.setOutputPath(job, new Path(args[3]));
+
+        MultipleInputs.addInputPath(job, new Path("C:///Users/ganer/Documents/classes2023/fall/Big_data/FaceIn.csv"),TextInputFormat.class, FaceInMapper.class);
+        MultipleInputs.addInputPath(job, new Path("C:///Users/ganer/Documents/classes2023/fall/Big_data/access.csv"),TextInputFormat.class, AccessMapper.class);
+        Path outputPath= new Path("hdfs://localhost:9000/project1/taskETest.txt");
+
+        FileOutputFormat.setOutputPath(job,outputPath);
+        outputPath.getFileSystem(conf).delete(outputPath);
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
