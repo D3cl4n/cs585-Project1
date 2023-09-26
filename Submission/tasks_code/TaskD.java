ac31f9d21b1b39ee607d9f8a80fd86dcff08d0a2
gbenderiya <143426706+gbenderiya@users.noreply.github.com>
2023-09-25 22:30:24 -0400

    Update TaskD.java
    remove header

--- Submission/tasks_code/TaskD.java
+++ Submission/tasks_code/TaskD.java
@@ -17,38 +17,30 @@ public class TaskD {
         private Text outvalue = new Text();
         public void map (Object key, Text value, Context context)
             throws IOException, InterruptedException{
-            if (!isHeader(value)) {
+
                 String[] parts = value.toString().split(",");
                 if (parts.length >= 2) {
-                    outkey.set(parts[0]);
-                    outvalue.set("Name\t" + parts[1]); //fk
+                    outkey.set(parts[0]);  // set key values
+                    outvalue.set("Name\t" + parts[1]); //set {'Name\t' + Name} as an output
                     context.write(outkey, outvalue);
                 }
-            }
 
             }
-        private boolean isHeader(Text value) {
-            return value.toString().startsWith("ID,Name,Nationality,Country Code,Hobby");
-        }
     }
     public static class AssociatesMapper extends Mapper<Object, Text, Text, Text> {
         private Text perId = new Text();
         private Text relID = new Text();
         public void map (Object key, Text value, Context context)
                 throws IOException, InterruptedException{
-            if (!isHeader(value)) {
                 String[] parts = value.toString().split(",");
                 if (parts.length >= 2) {
-                    perId.set(parts[1]);  // key 
-                    relID.set("RelCount\t" + parts[0]); //fk+relation id as value
+                    perId.set(parts[1]);  // set personid as a key
+                    relID.set("RelCount\t" + parts[0]); //set {'RelCount'+relation id} as value
                     context.write(perId, relID);
                 }
-            }
 
         }
-        private boolean isHeader(Text value) {
-            return value.toString().startsWith("FriendRel,PersonA_ID,PersonB_ID,DateOfFriendship,Desc");
-        }
+
     }
     public static class ReduceJoinReducer extends Reducer<Text, Text, Text, Text>{
         private Text pName = new Text();
@@ -56,18 +48,18 @@ public class TaskD {
         public void reduce(Text key, Iterable<Text> values, Context context)
             throws IOException, InterruptedException{
             String name = null;
-            int count = 0;
+            int count = 0;  // counter
 
             for (Text value : values) {
                 String[] parts = value.toString().split("\t");
                 if (parts.length == 2) {
-                    String dataType = parts[0];
+                    String keyType = parts[0];
                     String dataValue = parts[1];
 
-                    if (dataType.equals("Name")) {
-                        name = dataValue;
-                    } else if (dataType.equals("RelCount")) {
-                        count++;
+                    if (keyType.equals("Name")) {
+                        name = dataValue;  // assign name
+                    } else if (keyType.equals("RelCount")) {
+                        count++; // increase counter
                     }
                 }
             }
@@ -81,15 +73,14 @@ public class TaskD {
     }
     public static void main(String[] args) throws Exception {
         Configuration conf= new Configuration();
-        Job job=new Job(conf,"Task D");
+        Job job=new Job(conf,"Reduce Side Join");
         job.setJarByClass(TaskD.class);
         job.setReducerClass(ReduceJoinReducer.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(Text.class);
 
-        MultipleInputs.addInputPath(job, new Path("C:///Users/ganer/Documents/classes2023/fall/Big_data/FaceIn.csv"),TextInputFormat.class, FaceInMapper.class);
+        MultipleInputs.addInputPath(job, new Path("C:///Users/ganer/Documents/classes2023/fall/Big_data/facein.csv"),TextInputFormat.class, FaceInMapper.class);
         MultipleInputs.addInputPath(job, new Path("C:///Users/ganer/Documents/classes2023/fall/Big_data/associates.csv"),TextInputFormat.class, AssociatesMapper.class);
-        //MultipleInputs.addInputPath(job, new Path("hdfs://localhost:9000/project1/associates.csv"),TextInputFormat.class, AssociatesMapper.class);
         Path outputPath= new Path("hdfs://localhost:9000/project1/taskD.txt");
 
         FileOutputFormat.setOutputPath(job,outputPath);
