29544abbbd48bfdd796a469d53352819742ffea7
gbenderiya <143426706+gbenderiya@users.noreply.github.com>
2023-09-27 20:37:31 -0400

    Update facein.py
    removing header=false

--- Submission/data_generation_code/facein.py
+++ Submission/data_generation_code/facein.py
@@ -31,4 +31,4 @@ class FaceInData:
         FaceIn=pd.DataFrame(FaceIn,columns=['ID','Name','Nationality','Country Code','Hobby'])
         # FaceIn   
     
-        FaceIn.to_csv('facein.csv',sep=',',index=False,header=False)
+        FaceIn.to_csv('facein.csv',sep=',',index=False)
